e6f28bdb37436f3678911d6ea1708e8fc6d08900
sp8853 <103991911+sp8853@users.noreply.github.com>
2023-08-08 23:03:24 +0530

    Add files via upload

---
+++ lec3-2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//declare 2 variables and then assign them values of 3,5 and then add them 
+int a;
+int b;
+a=3;
+b=5;
+int c;
+c=a+b;
+cout<<c;
+}
